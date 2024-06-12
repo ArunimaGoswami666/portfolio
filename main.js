229c97a2e7efc8b3793d6b8a9053688e6b8df2fd
ArunimaGoswami666 <goswamisrija456@gmail.com>
2024-06-12 22:29:47 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+var typed=new Typed('#text',{
+    strings:['Shutter-Bug','Travel-lover','Software Developer'],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+    }); 
+    var typed=new Typed('#edu',{
+        strings:['Education','Qualifications'],
+        typeSpeed:100,
+        backSpeed:100,
+        backDelay:1000,
+        loop:true
+        }); 
+    
